1cc0d83dde9c1d73c6aa153b8d5b7f6728859d5f
Jan Kaluza <jkaluza@redhat.com>
2014-04-08 08:55:07 +0200

    Push only when there are new data in upstream repo

--- merge_with_upstream.sh
+++ merge_with_upstream.sh
@@ -2,8 +2,8 @@ git fetch upstream
 git checkout master
 git merge -s ours upstream/master 2>&1|grep "Already up-to-date."
 RET=$?
-git push
 if [ $RET != 0 ]; then
+	git push
 	make clean
 	autoreconf -f -i
 	./configure --disable-silent-rules
